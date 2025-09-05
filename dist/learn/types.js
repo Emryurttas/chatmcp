0ccb98c78b0d9d1e05106293a967be553039d4bc
Yurttas Emre <emre.yurttas@etudiant.univ-reims.fr>
2025-09-05 07:09:58 +0000

    Création du fichier types et ajout des annotations de types

---
+++ dist/learn/types.js
@@ -0,0 +1,8 @@
+"use strict";
+// Ajoutez les annotations de types pour les variables suivantes :
+Object.defineProperty(exports, "__esModule", { value: true });
+let firstName = "Bob";
+let age = 25;
+let isStudent = true;
+let marks = [9, 14.5, 18];
+//# sourceMappingURL=types.js.map
---
+++ src/learn/types.ts
@@ -0,0 +1,8 @@
+
+// Ajoutez les annotations de types pour les variables suivantes :
+
+let firstName: string = "Bob";
+let age: number = 25;
+let isStudent: boolean = true;
+let marks: number[] = [ 9, 14.5, 18 ];
+        
