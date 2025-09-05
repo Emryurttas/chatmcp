c5cd3439d24baed943ab780584fa7f0473c24efb
Yurttas Emre <emre.yurttas@etudiant.univ-reims.fr>
2025-09-05 07:22:34 +0000

    Ajout de la fonction dot, typage des paramètres

--- dist/learn/types.js
+++ dist/learn/types.js
@@ -5,4 +5,10 @@ let firstName = "Bob";
 let age = 25;
 let isStudent = true;
 let marks = [9, 14.5, 18];
+// Ajoutez les annotations de type à la déclaration de la fonction suivante et écrivez son code :
+function dotProduct(vector1, vector2) {
+    return (vector1.x * vector2.x) + (vector1.y * vector2.y);
+}
+console.log("dotProduct ---------------------------------------------------");
+console.log(dotProduct({ x: 3, y: 2 }, { x: 4, y: 5 })); // doit afficher 22
 //# sourceMappingURL=types.js.map
--- src/learn/types.ts
+++ src/learn/types.ts
@@ -6,3 +6,13 @@ let age: number = 25;
 let isStudent: boolean = true;
 let marks: number[] = [ 9, 14.5, 18 ];
         
+
+// Ajoutez les annotations de type à la déclaration de la fonction suivante et écrivez son code :
+
+function dotProduct(vector1: {x: number, y: number}, vector2: {x: number, y: number}) {
+    return (vector1.x * vector2.x) + (vector1.y * vector2.y)
+}
+
+console.log("dotProduct ---------------------------------------------------");
+console.log(dotProduct({ x : 3, y : 2 }, { x : 4, y : 5 })); // doit afficher 22
+        
