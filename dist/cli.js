6fbcb7315f9f181e17a31953d20a381efa49e8c1
Yurttas Emre <emre.yurttas@etudiant.univ-reims.fr>
2025-09-05 10:12:07 +0000

    fin de séance : modification de la méthode main pour le chatbot

--- dist/cli.js
+++ dist/cli.js
@@ -1,10 +1,23 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const chat_model_1 = require("./chat/chat.model");
+const promises_1 = require("readline/promises");
+const readLine = (0, promises_1.createInterface)({
+    input: process.stdin,
+    output: process.stdout
+});
 async function main() {
     const chat = new chat_model_1.ChatModel();
+    let isDone = false;
     const answer = await chat.send("Bonjour");
     console.log(answer);
+    const question = readLine.question("saisir");
+    while (isDone === false) {
+        const answer_next = await chat.send(await question);
+        if (answer_next == undefined) {
+            isDone = true;
+        }
+    }
 }
 main();
 //# sourceMappingURL=cli.js.map
--- src/cli.ts
+++ src/cli.ts
@@ -1,9 +1,24 @@
 import { ChatModel } from './chat/chat.model';
+import { createInterface } from "readline/promises";
+
+const readLine = createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
 async function main() {
     const chat = new ChatModel();
+    let isDone = false; 
     const answer = await chat.send("Bonjour");
-    console.log(answer);
+    console.log(answer); 
+    const question = readLine.question("saisir");
+    while(isDone === false){
+        const answer_next = await chat.send(await question);
+        if (answer_next == undefined)
+        {
+            isDone = true
+        }
+    }
 }
 
 main();
