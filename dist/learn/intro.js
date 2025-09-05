931fbf3249616663c2031406fa4de736ca3ef324
Yurttas Emre <emre.yurttas@etudiant.univ-reims.fr>
2025-09-05 06:54:29 +0000

    Création du fichier intro et observation des erreurs

---
+++ dist/learn/intro.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("--------------------------------------------------- addNumbers ");
+function addNumbers(x, y) {
+    return x + y;
+}
+console.log(addNumbers(3, 4));
+//# sourceMappingURL=intro.js.map
