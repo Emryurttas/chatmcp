11c0b8c09abb654d049253c0af819769872397e3
Yurttas Emre <emre.yurttas@etudiant.univ-reims.fr>
2025-09-05 07:41:56 +0000

    transformation de Vector2D en interfaces et constatations qu'il n'y a pas d'erreur

--- src/learn/types.ts
+++ src/learn/types.ts
@@ -1,4 +1,7 @@
-type Vector2D = {x: number, y: number};
+interface Vector2D{
+    x: number,
+    y: number
+}
 
 function dotProduct(vector1: Vector2D, vector2: Vector2D) {
     return (vector1.x * vector2.x) + (vector1.y * vector2.y) 
