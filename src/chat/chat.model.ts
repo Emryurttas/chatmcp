482ea7bf917d7ec9cb3700f0f8d2087cb9e5d5c2
Yurttas Emre <emre.yurttas@etudiant.univ-reims.fr>
2025-09-12 07:46:03 +0000

    Déclaration des propriétés et initialisation du constructeur

--- src/chat/chat.model.ts
+++ src/chat/chat.model.ts
@@ -1,9 +1,22 @@
 import { google } from '@ai-sdk/google';
 import { generateText } from 'ai';
+import { ChatRepository } from './chat.repository';
 
 const MODEL_NAME = google('gemini-2.0-flash');
 
+type ModelMessage = {
+    role: 'user';
+    content: string;
+}
+
 export class ChatModel {
+    private _chatId: string;
+    private static repository = new ChatRepository<ModelMessage>();
+
+    constructor() {
+        this._chatId = ChatModel.repository.create([]);
+    }
+
     async send(prompt: string): Promise<string> {
         
         const {text} = await generateText({
