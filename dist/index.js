e87ea7fe4798dcf428584daa0a65b98e209bcf0e
Yurttas Emre <emre.yurttas@etudiant.univ-reims.fr>
2025-09-19 06:24:45 +0000

    Modification de déclaration de la variable port

--- dist/index.js
+++ dist/index.js
@@ -5,7 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-const port = 7000;
+const port = process.env.PORT;
 app.get('/', (req, res) => {
     res.send('Bonjour');
 });
