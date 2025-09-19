8a1187a64dab24c6fed7c6d950a18662adb8d447
Yurttas Emre <emre.yurttas@etudiant.univ-reims.fr>
2025-09-19 07:24:11 +0000

    Ajout du css, icone et chargement de la bibliothèque htmx dans home

--- src/views/home.tsx
+++ src/views/home.tsx
@@ -1,16 +1,21 @@
-export function HomeView(props: { title: string }):JSX.Element {
-    return (<>
-        {'<!DOCTYPE html>'}
-        <html lang="fr">
-        <head>
-            <meta charset="utf-8"/>
-            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
-            <title>{ props.title }</title>
-        </head>
-        <body>
-            <h1>{ props.title }</h1>
-            <a href="/chat">Chatbot</a>
-        </body>
-        </html>
-    </>);
+export function HomeView(props: { title: string }): JSX.Element {
+    return (
+        <>
+            {'<!DOCTYPE html>'}
+            <html lang="fr">
+                <head>
+                    <meta charset="utf-8" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+                    <title>{props.title}</title>
+                    <link rel="stylesheet" href="/css/pico.min.css" />
+                    <link rel="icon" href="/images/bot.png" />
+                    <script src="https://iut-info.univ-reims.fr/users/nourrit/chatmcp/js/htmx.js"></script>
+                </head>
+                <body>
+                    <h1>{props.title}</h1>
+                    <a href="/chat">Chatbot</a>
+                </body>
+            </html>
+        </>
+    );
 }
