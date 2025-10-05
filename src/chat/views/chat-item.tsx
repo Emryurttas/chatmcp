3c24ec1039f4a4e2c942562f4c4c18448e27b43d
yurt0002 <emre.yurttas@etudiant.univ-reims.fr>
2025-10-05 17:27:47 +0200

    Création de la vue Chatitem

---
+++ src/chat/views/chat-item.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+interface ChatItemProps {
+    prompt: string;
+}
+
+export function ChatItemView({ prompt }: ChatItemProps) {
+    return (
+        <article className="chat-item">
+            <p>{prompt}</p>
+            <div>coucou</div>
+        </article>
+    );
+}
