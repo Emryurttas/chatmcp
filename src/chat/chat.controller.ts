f7d389c3b4f886abe44b23b63f247949d3b38e6c
yurt0002 <emre.yurttas@etudiant.univ-reims.fr>
2025-10-05 17:50:57 +0200

    Utilisation du chatitem

--- src/chat/chat.controller.ts
+++ src/chat/chat.controller.ts
@@ -1,21 +1,26 @@
 import { Request, Response } from 'express';
 import { ChatModel } from './chat.model';
-import { ChatView } from './views/chat'
+import { ChatView } from './views/chat';
+import { ChatItemView } from './views/chat-item';
 
 export class ChatController {
     public chat(req: Request, res: Response): void {
         const chatInstance = new ChatModel();
-        const conversationId = chatInstance.chatId
+        const conversationId = chatInstance.chatId;
 
-        const page = ChatView({ conversationId })
+        const page = ChatView({ conversationId });
 
         res.send(page);
     }
+
     public sendPrompt(req: Request, res: Response): void {
         const prompt = req.body.prompt;
         console.log(prompt);
 
-        res.send(`<p>${prompt}</p>`);
+        const chatItemHtml = ChatItemView({ prompt });
+
+        res.send(chatItemHtml);
     }
 }
+
 export const chatController = new ChatController();
--- src/chat/views/chat-item.tsx
+++ src/chat/views/chat-item.tsx
@@ -1,6 +1,4 @@
-import React from 'react';
-
-interface ChatItemProps {
+export interface ChatItemProps {
     prompt: string;
 }
 
