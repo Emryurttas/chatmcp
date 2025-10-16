f2db8c43ab76484912263c8193771bedfd43f9b6
yurt0002 <emre.yurttas@etudiant.univ-reims.fr>
2025-10-16 22:51:30 +0200

    ajout d'un spinner sur le DIV avec suppression après mise à jour

--- src/chat/views/chat-item.tsx
+++ src/chat/views/chat-item.tsx
@@ -8,10 +8,12 @@ export function ChatItemView({ prompt, id }: ChatItemProps) {
         <article className="chat-item">
             <p>{prompt}</p>
             <div
+                aria-busy="true"
                 hx-get={`/chat/query/${id}`}
                 hx-trigger="load"
                 hx-target="this"
                 hx-swap="innerHTML"
+                hx-on="afterSwap: this.removeAttribute('aria-busy')"
             ></div>
         </article>
     );
