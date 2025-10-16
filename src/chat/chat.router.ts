aefed98e57eef0cb0453b3a0e9bc5979cea3d1ea
yurt0002 <emre.yurttas@etudiant.univ-reims.fr>
2025-10-16 19:13:11 +0200

    Utilisation de validateParams dans les routes

--- src/chat/chat.router.ts
+++ src/chat/chat.router.ts
@@ -2,17 +2,23 @@ import { Router } from 'express';
 import { chatController } from './chat.controller';
 import { z } from 'zod';
 import { validateBody } from '../utils/validator';
+import { validateParams } from '../utils/validateParams';
 
 const router = Router();
 
 export const promptSchema = z.object({
-    prompt: z.string().min(1, "Le prompt ne peut pas être vide")
+    prompt: z.string().min(1, "Le prompt ne peut pas être vide"),
+});
+
+export const paramsSchema = z.object({
+    id: z.string().min(1, "L'identifiant de conversation est requis"),
 });
 
 router.get('/chat', chatController.chat.bind(chatController));
 
 router.post(
     '/chat/send/:id',
+    validateParams(paramsSchema),
     validateBody(promptSchema),
     chatController.sendPrompt.bind(chatController)
 );
