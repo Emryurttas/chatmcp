4ab2db5e317407ef5cffdfaa975912b9cd7da414
yurt0002 <emre.yurttas@etudiant.univ-reims.fr>
2025-10-16 19:21:28 +0200

    Ajout de la méthode query et utilisation du paramètre id dans sendprompt

--- src/chat/chat.controller.ts
+++ src/chat/chat.controller.ts
@@ -7,24 +7,24 @@ export class ChatController {
     public chat(req: Request, res: Response): void {
         const chatInstance = new ChatModel();
         const conversationId = chatInstance.chatId;
-
         const page = ChatView({ conversationId });
-
         res.send(page);
     }
 
     public sendPrompt(req: Request, res: Response): void {
         const prompt = req.body.prompt;
-        const conversationId = req.params.id;
-        console.log(conversationId);
-
+        const conversationId = req.params.id as string;
         const chatInstance = new ChatModel(conversationId);
-
         chatInstance.addPrompt(prompt);
-
-        const chatItemHtml = ChatItemView({ prompt });
-
+        const chatItemHtml = ChatItemView({ prompt, id: conversationId });
         res.send(chatItemHtml);
+    }
+
+    public async query(req: Request, res: Response): Promise<void> {
+        const conversationId = req.params.id;
+        const chatInstance = new ChatModel(conversationId);
+        const answer = await chatInstance.fetchAnswer();
+        res.send(answer);
     }
 }
 
