b2ace02568aa002c3db3759b1a00b5149313c7be
yurt0002 <emre.yurttas@etudiant.univ-reims.fr>
2025-10-16 23:43:51 +0200

    Ajout méthode stream

--- src/chat/chat.controller.ts
+++ src/chat/chat.controller.ts
@@ -49,6 +49,27 @@ export class ChatController {
         answer = answer.replace(/\n/g, 'RENDER-MD-LF');
         res.send(answer);
     }
+
+    public async stream(req: Request, res: Response): Promise<void> {
+        const conversationId = req.params.id as string;
+        const chatInstance = new ChatModel(conversationId);
+
+        res.setHeader('Content-Type', 'text/event-stream');
+        res.setHeader('Cache-Control', 'no-cache');
+        res.setHeader('Connection', 'keep-alive');
+        res.flushHeaders?.();
+
+        const stream = chatInstance.fetchAnswerStream();
+
+        for await (const token of stream) {
+            const formatted = token.replace(/\n/g, 'RENDER-MD-LF');
+            res.write(`event: token\ndata: ${formatted}\n\n`);
+        }
+
+        res.write(`event: close\ndata: \n\n`);
+        res.end();
+    }
 }
+
 
 export const chatController = new ChatController();
