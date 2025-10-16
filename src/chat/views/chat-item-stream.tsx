a9a43333b1985b46540af7d5393235af2e8b95c3
yurt0002 <emre.yurttas@etudiant.univ-reims.fr>
2025-10-16 23:36:16 +0200

    Ajout du vue chat-item-stream

---
+++ src/chat/views/chat-item-stream.tsx
@@ -0,0 +1,20 @@
+export interface ChatItemProps {
+    prompt: string;
+    id: string;
+}
+
+export function ChatItemStreamView({ prompt, id }: ChatItemProps) {
+    return (
+        <article className="chat-item">
+            <p>{prompt}</p>
+            <div
+                hx-ext="sse,render-markdown-stream"
+                sse-connect={`/chat/stream/${id}`}
+                sse-swap="token"
+                sse-close="close"
+                hx-target="this"
+                hx-swap="innerHTML"
+            ></div>
+        </article>
+    );
+}
