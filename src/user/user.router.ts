d39925091b86d14ac7da9ba8fcee6d03adc6f75c
Yurttas Emre <emre.yurttas@etudiant.univ-reims.fr>
2025-11-06 10:11:22 +0000

    Ajout de la route post login

--- src/user/user.router.ts
+++ src/user/user.router.ts
@@ -5,5 +5,7 @@ const userRouter = Router();
 
 userRouter.get('/user/all', userController.showUser);
 userRouter.get('/user/login', userController.loginForm)
+userRouter.post('/user/login', userController.login)
+
 
 export default userRouter;
