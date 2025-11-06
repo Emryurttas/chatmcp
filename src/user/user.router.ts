49c35befa89c959ec26804f8da944b7bedef041b
Yurttas Emre <emre.yurttas@etudiant.univ-reims.fr>
2025-11-06 10:00:51 +0000

    Ajout de la route get login

--- src/user/user.router.ts
+++ src/user/user.router.ts
@@ -3,7 +3,7 @@ import { userController } from './user.controller';
 
 const userRouter = Router();
 
-userRouter.get('/user/all', userController.showUser.bind(userController));
-
+userRouter.get('/user/all', userController.showUser);
+userRouter.get('/user/login', userController.loginForm)
 
 export default userRouter;
