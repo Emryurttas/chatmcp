b8d475aa844f38bb31f59b2781554a2fd27d8f74
Yurttas Emre <emre.yurttas@etudiant.univ-reims.fr>
2025-11-06 10:00:04 +0000

    Utilisation des jsx dans user

--- src/user/views/user.tsx
+++ src/user/views/user.tsx
@@ -2,21 +2,24 @@
 import { User } from "../user";
 import { idAsString } from "../../utils/id-as-string";
 
-export function UserView({ users }: { users: User[] }): string {
-    return `
-    <html lang="fr">
-      <head><title>Liste des utilisateurs</title></head>
-      <body>
-        <h1>Utilisateurs</h1>
-        <ul>
-          ${users.map(u => `
-            <li>
-              <strong>${u.userName}</strong> (${u.email}) 
-              — id: ${idAsString(u._id)}
-            </li>
-          `).join('')}
-        </ul>
-      </body>
-    </html>
-  `;
+export function UserView({ users }: { users: User[] }): JSX.Element {
+    return (
+    <>
+      {'<!DOCTYPE html>'}
+      <html lang="fr">
+        <head><title>Liste des utilisateurs</title></head>
+        <body>
+          <h1>Utilisateurs</h1>
+          <ul>
+            ${users.map(u => `
+              <li>
+                <strong>${u.userName}</strong> (${u.email}) 
+                — id: ${idAsString(u._id)}
+              </li>
+            `).join('')}
+          </ul>
+        </body>
+      </html>
+    </>
+    );
 }
