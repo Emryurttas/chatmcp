268531180c485b2802ceeaa8bee919c319b49326
Yurttas Emre <emre.yurttas@etudiant.univ-reims.fr>
2025-11-11 17:36:46 +0000

    utilisation de zod et validator

--- src/user/user.router.ts
+++ src/user/user.router.ts
@@ -1,11 +1,23 @@
 import { Router } from 'express';
 import { userController } from './user.controller';
+import { z } from 'zod';
+import { validateBody } from '../utils/validator';
 
 const userRouter = Router();
 
-userRouter.get('/user/all', userController.showUser);
-userRouter.get('/user/login', userController.loginForm)
-userRouter.post('/user/login', userController.login)
+const loginSchema = z.object({
+  userName: z.string().min(1, "Le nom d'utilisateur est requis"),
+  password: z.string().min(1, "Mot de passe requis"),
+});
 
+userRouter.get('/user/all', userController.showUser.bind(userController));
+
+userRouter.get('/user/login', userController.loginForm.bind(userController));
+
+userRouter.post(
+  '/user/login',
+  validateBody(loginSchema),
+  userController.login.bind(userController)
+);
 
 export default userRouter;
