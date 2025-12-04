16dc9abc7b43c39ebef3dc0cb471930bf4ea0578
Yurttas Emre <emre.yurttas@etudiant.univ-reims.fr>
2025-12-04 09:26:14 +0000

    Ajout de la méthode updateEmail

--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -1,5 +1,6 @@
 import { mongodb } from "../services/mongo";
 import { User } from "./user";
+import { ObjectId } from "bson";
 
 class UserRepository {
     private readonly collection = mongodb.collection<User>('users');
@@ -7,9 +8,18 @@ class UserRepository {
     async findAll(): Promise<User[]> {
         return this.collection.find().toArray();
     }
-    async findByUserName(userName: string){
-        return this.collection.findOne({userName})
+
+    async findByUserName(userName: string): Promise<User | null> {
+        return this.collection.findOne({ userName });
+    }
+
+    async updateEmail(id: string | ObjectId, email: string): Promise<void> {
+        const _id = typeof id === "string" ? new ObjectId(id) : id;
+        await this.collection.updateOne(
+            { _id },
+            { $set: { email } }
+        );
     }
 }
 
-export const userRepository = new UserRepository;
+export const userRepository = new UserRepository();
