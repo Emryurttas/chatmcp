63545237e3b106601bc8fe24a9036bdebfd7dfd1
Yurttas Emre <emre.yurttas@etudiant.univ-reims.fr>
2025-12-05 21:30:01 +0000

    Création du schéma chat

---
+++ src/chat/chat.schema.ts
@@ -0,0 +1,56 @@
+import { SchemaManager } from "../utils/schema-manager";
+
+const chatSchema = {
+    $jsonSchema: {
+        bsonType: "object",
+        required: ["userId", "title", "creationDate", "lastModificationDate", "messages"],
+        properties: {
+            _id: {
+                bsonType: "objectId",
+                description: "Unique identifier of the chat document"
+            },
+
+            userId: {
+                bsonType: "objectId",
+                description: "'userId' is required and must be an ObjectId"
+            },
+
+            title: {
+                bsonType: "string",
+                description: "'title' is required and must be a string"
+            },
+
+            creationDate: {
+                bsonType: "date",
+                description: "'creationDate' is required and must be a date"
+            },
+
+            lastModificationDate: {
+                bsonType: "date",
+                description: "'lastModificationDate' is required and must be a date"
+            },
+
+            messages: {
+                bsonType: "array",
+                description: "'messages' must be an array of message objects",
+                items: {
+                    bsonType: "object",
+                    required: ["role", "content"],
+                    properties: {
+                        role: {
+                            enum: ["user", "bot"],
+                            description: "'role' must be one of: user or bot"
+                        },
+                        content: {
+                            description: "'content' is required (type intentionally not specified)"
+                        }
+                    }
+                }
+            }
+        }
+    }
+};
+
+new SchemaManager("chats", chatSchema)
+    .executeCommand(process.argv)
+    .then(() => process.exit(0));
