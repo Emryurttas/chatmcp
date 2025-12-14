89ba3dd9530bab7f7a339768d7a3e831c49b3957
Yurttas Emre <emre.yurttas@etudiant.univ-reims.fr>
2025-12-14 16:56:36 +0000

    Ajout bouton recherche

--- src/chat/views/chatCount.tsx
+++ src/chat/views/chatCount.tsx
@@ -3,11 +3,23 @@ import { PropsWithChildren } from "@kitajs/html";
 export function ChatCount(props: PropsWithChildren<{ count: number }>): JSX.Element {
     return (
         <div style={{
-            textAlign: 'center',
-            flexGrow: 1,
-            color: 'white',
-        }}>
-            {props.count} conversation{props.count > 1 ? 's' : ''}
+                textAlign: 'center',
+                flexGrow: 1,
+                color: 'white',
+            }}
+        >
+            <span>{props.count} conversation{props.count > 1 ? 's' : ''}</span>
+
+            <button
+                type="button"
+                class="btn-search"
+                hx-get="/chat/searchForm"
+                hx-target="#chat-count"
+                hx-swap="outerHTML"
+                title="Rechercher un titre"
+            >
+                <i class="fas fa-search"></i>
+            </button>
         </div>
     );
 }
