8a6f83c7bb4ac1d9db7abc81b14c7e7948969475
Yurttas Emre <emre.yurttas@etudiant.univ-reims.fr>
2025-12-14 20:52:09 +0000

    gérer l'événement info

--- public/js/discuss.js
+++ public/js/discuss.js
@@ -2,3 +2,15 @@
 // @ts-nocheck
 /* eslint-disable no-undef */
 const socket = io(window.location.origin);
+
+socket.on('info', (message) => {
+    console.log('Info reçue du serveur :', message);
+
+    const chatDiv = document.getElementById('chat');
+    if (chatDiv) {
+        const article = document.createElement('article');
+        article.className = 'info-message';
+        article.textContent = message;
+        chatDiv.appendChild(article);
+    }
+});
