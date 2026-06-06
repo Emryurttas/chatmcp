a053405f3f8b06ec3c2e85537ff03a8f9bb88d05
yurt0002 <emre.yurttas@etudiant.univ-reims.fr>
2026-06-07 01:13:23 +0200

    fix: add TLS options for MongoDB Atlas connection

--- src/services/mongo.ts
+++ src/services/mongo.ts
@@ -1,6 +1,9 @@
 import { MongoClient } from "mongodb";
 
 const uri = process.env.MONGO_URI ?? (() => { throw new Error("MONGO_URI is not defined"); })();
-const client = new MongoClient(uri);
+const client = new MongoClient(uri, {
+    tls: true,
+    tlsAllowInvalidCertificates: false,
+});
 
 export const mongodb = client.db(process.env.MONGO_DB, { ignoreUndefined: true });
