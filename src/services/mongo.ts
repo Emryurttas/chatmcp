bf88eb60293d39d9e127bc26d09c2cd38afa85de
yurt0002 <emre.yurttas@etudiant.univ-reims.fr>
2026-06-07 01:27:07 +0200

    fix: allow invalid TLS certs for Atlas on Render

--- src/services/mongo.ts
+++ src/services/mongo.ts
@@ -3,7 +3,7 @@ import { MongoClient } from "mongodb";
 const uri = process.env.MONGO_URI ?? (() => { throw new Error("MONGO_URI is not defined"); })();
 const client = new MongoClient(uri, {
     tls: true,
-    tlsAllowInvalidCertificates: false,
+    tlsAllowInvalidCertificates: true,
 });
 
 export const mongodb = client.db(process.env.MONGO_DB, { ignoreUndefined: true });
